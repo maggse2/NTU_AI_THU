81baea53576fb44b0dc043824911bb1297e8bf2c
Max Paulenz <paulenz.max@gmail.com>
2024-02-22 13:36:11 +0100

    edited filename

--- HW0/HW0_paulenz.py
+++
@@ -1,2 +0,0 @@
-# This project was created by Max Paulenz for the purpose of the HW0 assignment in CSIE5400-02
-# Student ID a12921301
---
+++ HW0/a12921301_hw0.py
@@ -0,0 +1,25 @@
+# This project was created by Max Paulenz for the purpose of the HW0 assignment in CSIE5400-02
+# Student ID a12921301
+
+# Preparation
+# Islands will be referred to as Nodes and Bridges as Edges
+
+# Translation of the rules
+# 1 & 2 Each node should not be in the path more than once
+# 3 When multiple Nodes are available, always choose the one with the lowest index
+# 4 & 5 All Nodes are equal
+
+# Find the longest continous path in the graph
+
+class Solution:
+    def __init__(self):
+        pass
+        
+    def solve(self):
+        pass
+    
+    # Feel free to define your own member function
+
+if __name__ == '__main__':
+    ans = Solution()
+    ans.solve()
